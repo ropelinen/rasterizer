74e763a68568dccdf844f52582285647a69a9d45
ropelinen <ropelinen@gmail.com>
2015-12-18 17:07:12 +0200

    Added a succes message and pause to the end of the build_rasterizer so that I don't need to run it from the command line to see the results. Also added new lines to the end of the build bats.

--- tls/build_rasterizer.bat
+++ tls/build_rasterizer.bat
@@ -2,6 +2,8 @@ call build.bat ../src/software_rasterizer.sln x86
 if errorlevel 1 goto fail_x86
 call build.bat ../src/software_rasterizer.sln x64
 if errorlevel 1 goto fail_x64
+
+echo "All build succeeded"
 goto end
 
 :fail_x86
@@ -13,3 +15,4 @@ echo "Failed to build x64"
 goto end
 
 :end
+pause
