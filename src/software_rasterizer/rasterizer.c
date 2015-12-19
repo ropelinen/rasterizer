20b512f7ec7d549cc0edb47739f2ca54d0701dc6
ropelinen <ropelinen@gmail.com>
2015-12-19 18:23:00 +0200

    Rasterizer now uses vec3_float vertex buffer instead of a vec2_int buffer.

--- src/software_rasterizer/rasterizer.c
+++ src/software_rasterizer/rasterizer.c
@@ -14,7 +14,7 @@ int orient2d(const struct vec2_int *p1, const struct vec2_int *p2, const struct 
 	return (p2->x - p1->x)*(p3->y - p1->y) - (p2->y - p1->y)*(p3->x - p1->x);
 }
 
-void rasterizer_rasterize_triangle(uint32_t *render_target, const struct vec2_int *target_size, const struct vec2_int *vert_buf, const unsigned int *ind_buf, const unsigned int index_count)
+void rasterizer_rasterize_triangle(uint32_t *render_target, const struct vec2_int *target_size, const struct vec3_float *vert_buf, const unsigned int *ind_buf, const unsigned int index_count)
 {
 	assert(render_target && "rasterizer_rasterize_triangle: render_target is NULL");
 	assert(target_size && "rasterizer_rasterize_triangle: target_size is NULL");
@@ -28,14 +28,21 @@ void rasterizer_rasterize_triangle(uint32_t *render_target, const struct vec2_in
 
 	for (unsigned int i = 0; i < index_count; i += 3)
 	{
-		const struct vec2_int *p1 = &vert_buf[ind_buf[i]];
-		const struct vec2_int *p2 = &vert_buf[ind_buf[i + 1]];
-		const struct vec2_int *p3 = &vert_buf[ind_buf[i + 2]];
+		/* Very crude, needs sub-pixel precision */
+		struct vec2_int p1;
+		p1.x = (int)vert_buf[ind_buf[i]].x;
+		p1.y = (int)vert_buf[ind_buf[i]].y;
+		struct vec2_int p2;
+		p2.x = (int)vert_buf[ind_buf[i + 1]].x;
+		p2.y = (int)vert_buf[ind_buf[i + 1]].y;
+		struct vec2_int p3;
+		p3.x = (int)vert_buf[ind_buf[i + 2]].x;
+		p3.y = (int)vert_buf[ind_buf[i + 2]].y;
 
-		min.x = min3(p1->x, p2->x, p3->x);
-		min.y = min3(p1->y, p2->y, p3->y);
-		max.x = max3(p1->x, p2->x, p3->x);
-		max.y = max3(p1->y, p2->y, p3->y);
+		min.x = min3(p1.x, p2.x, p3.x);
+		min.y = min3(p1.y, p2.y, p3.y);
+		max.x = max3(p1.x, p2.x, p3.x);
+		max.y = max3(p1.y, p2.y, p3.y);
 
 		/* Clip to screen */
 		int half_width = target_size->x / 2;
@@ -52,9 +59,9 @@ void rasterizer_rasterize_triangle(uint32_t *render_target, const struct vec2_in
 			for (point.x = min.x; point.x <= max.x; ++point.x)
 			{
 				/* Calculate barysentric coords */
-				int w0 = orient2d(p1, p2, &point);
-				int w1 = orient2d(p2, p3, &point);
-				int w2 = orient2d(p3, p1, &point);
+				int w0 = orient2d(&p1, &p2, &point);
+				int w1 = orient2d(&p2, &p3, &point);
+				int w2 = orient2d(&p3, &p1, &point);
 
 				if (w0 >= 0 && w1 >= 0 && w2 >= 0)
 				{
