c38affea960e0743c7645472e8c851ddf7df3fdc
ropelinen <ropelinen@gmail.com>
2015-12-19 18:21:54 +0200

    vec3 structs

--- src/software_rasterizer/vector.h
+++ src/software_rasterizer/vector.h
@@ -6,9 +6,19 @@ struct vec2_float
 	float x, y;
 };
 
+struct vec3_float
+{
+	float x, y, z;
+};
+
 struct vec2_int
 {
 	int x, y;
+};
+
+struct vec3_int
+{
+	int x, y, z;
 };
 
 #endif /* RPLNN_VECTOR_H */
