2b09d1bf4b915b8bd07679873b05213d58e42fea
ropelinen <ropelinen@gmail.com>
2015-12-19 18:02:37 +0200

    Fixed a typo in the build_rasterizer.bat

--- tls/build_rasterizer.bat
+++ tls/build_rasterizer.bat
@@ -3,7 +3,7 @@ if errorlevel 1 goto fail_x86
 call build.bat ../src/software_rasterizer.sln x64
 if errorlevel 1 goto fail_x64
 
-echo "All build succeeded"
+echo "All builds succeeded"
 goto end
 
 :fail_x86
