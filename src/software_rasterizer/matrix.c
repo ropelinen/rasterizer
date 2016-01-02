45eaddce26436c2457a50c78a7b0c0b1af15a6d4
ropelinen <ropelinen@gmail.com>
2016-01-02 18:03:40 +0200

    Added 4x4 matrix and related functions. - mul with mat34 and vec3 - two perspective projection matrix generation functions.

--- src/software_rasterizer/matrix.c
+++ src/software_rasterizer/matrix.c
@@ -164,3 +164,77 @@ struct matrix_3x4 mat34_mul_mat34(const struct matrix_3x4 *mat1, const struct ma
 
 	return result;
 }
+
+struct matrix_4x4 mat44_get_perspective_lh_fov(float fovy_rad, float aspect, float near, float far)
+{
+	struct matrix_4x4 result;
+
+	float scaley = 1.0f / (tanf(fovy_rad / 2.0f));
+	float scalex = scaley / aspect;
+	float c = far / (far - near);
+
+	result.mat[0][0] = scalex; result.mat[0][1] = 0.0f;   result.mat[0][2] = 0.0f; result.mat[0][3] = 0.0f;
+	result.mat[1][0] = 0.0f;   result.mat[1][1] = scaley; result.mat[1][2] = 0.0f; result.mat[1][3] = 0.0f;
+	result.mat[2][0] = 0.0f;   result.mat[2][1] = 0.0f;   result.mat[2][2] = c;    result.mat[2][3] = -near * c;
+	result.mat[3][0] = 0.0f;   result.mat[3][1] = 0.0f;   result.mat[3][2] = 1.0f; result.mat[3][3] = 0.0f;
+
+	return result;
+}
+struct matrix_4x4 mat44_get_perspective_lh(float width, float height, float near, float far)
+{
+	struct matrix_4x4 result;
+	
+	float d_n = 2.0f * near;
+	float f_n = far - near;
+	result.mat[0][0] = d_n  / width;   result.mat[0][1] = 0.0f;         result.mat[0][2] = 0.0f;      result.mat[0][3] = 0.0f;
+	result.mat[1][0] = 0.0f;           result.mat[1][1] = d_n / height; result.mat[1][2] = 0.0f;      result.mat[1][3] = 0.0f;
+	result.mat[2][0] = 0.0f;           result.mat[2][1] = 0.0f;         result.mat[2][2] = far / f_n; result.mat[2][3] = (near * far) / (near - far);
+	result.mat[3][0] = 0.0f;           result.mat[3][1] = 0.0f;         result.mat[3][2] = 1.0f;      result.mat[3][3] = 0.0f;
+
+	return result;
+}
+
+struct vec4_float mat44_mul_vec3(const struct matrix_4x4 *mat, const struct vec3_float *vec)
+{
+	assert(mat && "mat44_mul_vec3: mat is NULL");
+	assert(vec && "mat44_mul_vec3: vec is NULL");
+
+	struct vec4_float result;
+
+	result.x = (mat->mat[0][0] * vec->x) + (mat->mat[0][1] * vec->y) + (mat->mat[0][2] * vec->z) + (mat->mat[0][3]/* * 1 */);
+	result.y = (mat->mat[1][0] * vec->x) + (mat->mat[1][1] * vec->y) + (mat->mat[1][2] * vec->z) + (mat->mat[1][3]/* * 1 */);
+	result.z = (mat->mat[2][0] * vec->x) + (mat->mat[2][1] * vec->y) + (mat->mat[2][2] * vec->z) + (mat->mat[2][3]/* * 1 */);
+	result.w = (mat->mat[3][0] * vec->x) + (mat->mat[3][1] * vec->y) + (mat->mat[3][2] * vec->z) + (mat->mat[3][3]/* * 1 */);
+
+	return result;
+}
+
+struct matrix_4x4 mat44_mul_mat34(const struct matrix_4x4 *mat1, const struct matrix_3x4 *mat2)
+{
+	assert(mat1 && "mat44_mul_mat34: mat1 is NULL");
+	assert(mat2 && "mat44_mul_mat34: mat2 is NULL");
+
+	struct matrix_4x4 result;
+
+	result.mat[0][0] = mat1->mat[0][0] * mat2->mat[0][0] + mat1->mat[0][1] * mat2->mat[1][0] + mat1->mat[0][2] * mat2->mat[2][0] /* + mat1->mat[0][3] * 0 */;
+	result.mat[0][1] = mat1->mat[0][0] * mat2->mat[0][1] + mat1->mat[0][1] * mat2->mat[1][1] + mat1->mat[0][2] * mat2->mat[2][1] /* + mat1->mat[0][3] * 0 */;
+	result.mat[0][2] = mat1->mat[0][0] * mat2->mat[0][2] + mat1->mat[0][1] * mat2->mat[1][2] + mat1->mat[0][2] * mat2->mat[2][2] /* + mat1->mat[0][3] * 0 */;
+	result.mat[0][3] = mat1->mat[0][0] * mat2->mat[0][3] + mat1->mat[0][1] * mat2->mat[1][3] + mat1->mat[0][2] * mat2->mat[2][3] + mat1->mat[0][3] /* * 1 */;
+
+	result.mat[1][0] = mat1->mat[1][0] * mat2->mat[0][0] + mat1->mat[1][1] * mat2->mat[1][0] + mat1->mat[1][2] * mat2->mat[2][0] /* + mat1->mat[1][3] * 0 */;
+	result.mat[1][1] = mat1->mat[1][0] * mat2->mat[0][1] + mat1->mat[1][1] * mat2->mat[1][1] + mat1->mat[1][2] * mat2->mat[2][1] /* + mat1->mat[1][3] * 0 */;
+	result.mat[1][2] = mat1->mat[1][0] * mat2->mat[0][2] + mat1->mat[1][1] * mat2->mat[1][2] + mat1->mat[1][2] * mat2->mat[2][2] /* + mat1->mat[1][3] * 0 */;
+	result.mat[1][3] = mat1->mat[1][0] * mat2->mat[0][3] + mat1->mat[1][1] * mat2->mat[1][3] + mat1->mat[1][2] * mat2->mat[2][3] + mat1->mat[1][3] /* * 1 */;
+
+	result.mat[2][0] = mat1->mat[2][0] * mat2->mat[0][0] + mat1->mat[2][1] * mat2->mat[1][0] + mat1->mat[2][2] * mat2->mat[2][0] /* + mat1->mat[2][3] * 0 */;
+	result.mat[2][1] = mat1->mat[2][0] * mat2->mat[0][1] + mat1->mat[2][1] * mat2->mat[1][1] + mat1->mat[2][2] * mat2->mat[2][1] /* + mat1->mat[2][3] * 0 */;
+	result.mat[2][2] = mat1->mat[2][0] * mat2->mat[0][2] + mat1->mat[2][1] * mat2->mat[1][2] + mat1->mat[2][2] * mat2->mat[2][2] /* + mat1->mat[2][3] * 0 */;
+	result.mat[2][3] = mat1->mat[2][0] * mat2->mat[0][3] + mat1->mat[2][1] * mat2->mat[1][3] + mat1->mat[2][2] * mat2->mat[2][3] + mat1->mat[2][3] /* * 1 */;
+
+	result.mat[3][0] = mat1->mat[3][0] * mat2->mat[0][0] + mat1->mat[3][1] * mat2->mat[1][0] + mat1->mat[3][2] * mat2->mat[2][0] /* + mat1->mat[3][3] * 0 */;
+	result.mat[3][1] = mat1->mat[3][0] * mat2->mat[0][1] + mat1->mat[3][1] * mat2->mat[1][1] + mat1->mat[3][2] * mat2->mat[2][1] /* + mat1->mat[3][3] * 0 */;
+	result.mat[3][2] = mat1->mat[3][0] * mat2->mat[0][2] + mat1->mat[3][1] * mat2->mat[1][2] + mat1->mat[3][2] * mat2->mat[2][2] /* + mat1->mat[3][3] * 0 */;
+	result.mat[3][3] = mat1->mat[3][0] * mat2->mat[0][3] + mat1->mat[3][1] * mat2->mat[1][3] + mat1->mat[3][2] * mat2->mat[2][3] + mat1->mat[3][3] /* * 1 */;
+
+	return result;
+}
--- src/software_rasterizer/matrix.h
+++ src/software_rasterizer/matrix.h
@@ -6,6 +6,11 @@ struct matrix_3x4
 	float mat[3][4];
 };
 
+struct matrix_4x4
+{
+	float mat[4][4];
+};
+
 float mat34_get_det(const struct matrix_3x4 *mat);
 struct matrix_3x4 mat34_get_inverse(const struct matrix_3x4 *mat);
 
@@ -18,5 +23,10 @@ struct matrix_3x4 mat34_get_translation(const struct vec3_float *translation);
 struct matrix_3x4 mat34_mul_scal(const struct matrix_3x4 *mat, float scal);
 struct vec3_float mat34_mul_vec3(const struct matrix_3x4 *mat, const struct vec3_float *vec);
 struct matrix_3x4 mat34_mul_mat34(const struct matrix_3x4 *mat1, const struct matrix_3x4 *mat2);
+
+struct matrix_4x4 mat44_get_perspective_lh_fov(float fovy_rad, float aspect, float near, float far);
+struct matrix_4x4 mat44_get_perspective_lh(float width, float height, float near, float far);
+struct vec4_float mat44_mul_vec3(const struct matrix_4x4 *mat, const struct vec3_float *vec);
+struct matrix_4x4 mat44_mul_mat34(const struct matrix_4x4 *mat1, const struct matrix_3x4 *mat2);
 
 #endif /* RPLNN_MATRIX_H */
