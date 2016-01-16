15ae4bea98dec9b6e2e0fae6ce1e711a2842b089
ropelinen <ropelinen@gmail.com>
2016-01-16 20:19:01 +0200

    Added a texture implementation (uses stb_image).

---
+++ src/software_rasterizer/demo/texture.c
@@ -0,0 +1,56 @@
+#include "software_rasterizer/precompiled.h"
+
+#include "font.h"
+
+#include "software_rasterizer/vector.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb\stb_image.h>
+
+struct texture
+{
+	uint32_t *buf;
+	struct vec2_int size;
+};
+
+struct texture *texture_create(const char *file_name)
+{
+	assert(file_name && "texture_create: file_name is NULL");
+
+	struct texture *texture = malloc(sizeof(struct texture));
+
+	int n;
+	unsigned char *data = stbi_load(file_name, &texture->size.x, &texture->size.y, &n, 3);
+	if (!data)
+	{
+		free(texture);
+		return NULL;
+	}
+
+	texture->buf = malloc(texture->size.x * texture->size.y * sizeof(uint32_t));
+	for (int i = 0, j = 0; i < texture->size.x * texture->size.y; ++i, j += 3)
+		texture->buf[i] = (data[j] << 16) | (data[j + 1] << 8) | data[j + 2];
+
+	stbi_image_free(data);
+
+	return texture;
+}
+
+void texture_destroy(struct texture **texture)
+{
+	assert(texture && "texture_destroy: texture is NULL");
+	assert(*texture && "texture_destroy: *texture is NULL");
+
+	free((*texture)->buf);
+	free(*texture);
+}
+
+void texture_get_info(struct texture *texture, uint32_t **buf, struct vec2_int **size)
+{
+	assert(texture && "texture_get_info: texture is NULL");
+	assert(buf && "texture_get_info: buf is NULL");
+	assert(size && "texture_get_info: size is NULL");
+
+	*buf = texture->buf;
+	*size = &(texture->size);
+}
---
+++ src/software_rasterizer/demo/texture.h
@@ -0,0 +1,14 @@
+#ifndef RPLNN_TEXTURE_H
+#define RPLNN_TEXTURE_H
+
+struct texture;
+
+/* Should create a version of this which doesn't malloc (basically just give memory block as a parameter). */
+struct texture *texture_create(const char *file_name);
+
+void texture_destroy(struct texture **texture);
+
+/* Just create a stryct vec2_int texture_get_size func */
+void texture_get_info(struct texture *texture, uint32_t **buf, struct vec2_int **size);
+
+#endif /* RPLNN_TEXTURE_H */
