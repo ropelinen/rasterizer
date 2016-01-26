da5f70fd1c342a87c704f98ea533301ce96e2b99
ropelinen <ropelinen@gmail.com>
2016-01-26 19:00:08 +0200

    Clean up 5: Added parameter names to function declaration.

--- src/software_rasterizer/demo/osal.h
+++ src/software_rasterizer/demo/osal.h
@@ -9,7 +9,7 @@ struct renderer_info;
 void error_popup(const char *msg, const bool kill_program);
 
 /* This should be defined in main.c and only containt non platform specific code */
-void main(struct api_info *, struct renderer_info *);
+void main(struct api_info *api_info, struct renderer_info *renderer_info);
 
 bool event_loop(void);
 
