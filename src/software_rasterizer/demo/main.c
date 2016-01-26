9a85b273553b0e92bd65505672070719754cf9ac
ropelinen <ropelinen@gmail.com>
2016-01-26 20:08:10 +0200

    Clean up 9: Moved stuff from main() to new functions.

--- src/software_rasterizer/demo/main.c
+++ src/software_rasterizer/demo/main.c
@@ -9,8 +9,11 @@
 #define VERTS_IN_BOX 14
 #define LARGE_VERT_BUF_BOXES 8
 
+void transform_vertices(const struct vec3_float *in_verts, struct vec4_float *out_verts, unsigned int vert_count,
+                     struct matrix_3x4 *translation, struct matrix_3x4 *rotation, struct matrix_4x4 *camera_projection);
 void handle_input(struct api_info *api_info, float dt, struct vec3_float *camera_trans);
 
+void create_box_buffers(struct vec3_float *out_vert_buf, struct vec2_float *out_uv_buf, unsigned int *out_ind_buf);
 void generate_large_test_buffers(const struct vec3_float *vert_buf_box, const struct vec2_float *uv_box, const unsigned int *ind_buf_box,
 	struct vec3_float *out_vert_buf, struct vec2_float *out_uv, unsigned int *out_ind_buf, const struct vec3_float *box_offsets, const unsigned int box_count_out);
 
@@ -48,61 +51,49 @@ void main(struct api_info *api_info, struct renderer_info *renderer_info)
 	unsigned int stabilizing_delay = 500;
 
 	/* Test box, CCW */
-	struct vec3_float vert_buf[VERTS_IN_BOX] = { { .x = -2.0f, .y = 2.0f, .z = 2.0f }, { .x = -2.0f, .y = -2.0f, .z = 2.0f }, { .x = -2.0f, .y = 2.0f, .z = 2.0f },
-	                                   { .x = -2.0f, .y = 2.0f, .z = -2.0f }, { .x = -2.0f, .y = -2.0f, .z = -2.0f }, { .x = -2.0f, .y = -2.0f, .z = 2.0f },
-	                                   { .x = 2.0f, .y = 2.0f, .z = 2.0f }, { .x = 2.0f, .y = 2.0f, .z = -2.0f }, { .x = 2.0f, .y = -2.0f, .z = -2.0f },
-	                                   { .x = 2.0f, .y = -2.0f, .z = 2.0f }, { .x = 2.0f, .y = 2.0f, .z = 2.0f }, { .x = 2.0f, .y = -2.0f, .z = 2.0f },
-	                                   { .x = -2.0f, .y = 2.0f, .z = 2.0f }, { .x = -2.0f, .y = -2.0f, .z = 2.0f } };
-	struct vec4_float final_vert_buf[VERTS_IN_BOX];
-	struct vec4_float final_vert_buf2[VERTS_IN_BOX];
-
-	struct vec2_float uv[VERTS_IN_BOX] = { { .x = 0.0f, .y = 0.33f }, { .x = 0.0f, .y = 0.66f }, { .x = 0.25f, .y = 0.0f }, { .x = 0.25f, .y = 0.33f },
-	                             { .x = 0.25f, .y = 0.66f }, { .x = 0.25f, .y = 1.0f }, { .x = 0.5f, .y = 0.0f }, { .x = 0.5f, .y = 0.33f }, 
-	                             { .x = 0.5f, .y = 0.66f }, { .x = 0.5f, .y = 1.0f }, { .x = 0.75f, .y = 0.33f }, { .x = 0.75f, .y = 0.66f }, 
-	                             { .x = 1.0f, .y = 0.33f }, { .x = 1.0f, .y = 0.66f } };
-	unsigned int ind_buf[36] = { 3, 4, 7, 7, 4, 8 /* front */
-	                           , 2, 3, 6, 6, 3, 7 /* top */
-	                           , 4, 5, 8, 8, 5, 9 /* bottom */
-	                           , 0, 1, 3, 3, 1, 4 /* left */
-	                           , 7, 8, 10, 10, 8, 11 /* right */
-	                           , 10, 11, 12, 12, 11, 13 }; /* back */
+	struct vec3_float vert_buf[VERTS_IN_BOX];
+	struct vec2_float uv[VERTS_IN_BOX];
+	unsigned int ind_buf[36];
+	const unsigned int ind_buf_size = sizeof(ind_buf) / sizeof(ind_buf[0]);
+	create_box_buffers(&(vert_buf[0]), &(uv[0]), &(ind_buf[0]));
 
 	struct vec3_float vert_buf_large[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
-	struct vec4_float final_vert_buf_large[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
-	struct vec4_float final_vert_buf_large2[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
-	struct vec4_float final_vert_buf_large3[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
 	struct vec2_float uv_large[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
 	unsigned int ind_buf_large[36 * LARGE_VERT_BUF_BOXES];
+	const unsigned int ind_buf_large_size = sizeof(ind_buf_large) / sizeof(ind_buf_large[0]);
 	struct vec3_float box_offsets[LARGE_VERT_BUF_BOXES] = { { .x = -6.0f, .y = 6.0f, .z = 6.0f }, { .x = 2.0f, .y = 4.0f, .z = 6.0f }, { .x = -2.0f, .y = -2.0f, .z = 2.0f }, { .x = 0.0f, .y = 0.0f, .z = 0.0f },
 	                                                        { .x = -6.0f, .y = 2.0f, .z = -2.0f }, { .x = -4.0f, .y = -4.0f, .z = -8.0f }, { .x = 4.0f, .y = 6.0f, .z = -6.0f }, { .x = 2.0f, .y = 10.0f, .z = -8.0f } };
 	generate_large_test_buffers(&vert_buf[0], &uv[0], &ind_buf[0], &vert_buf_large[0], &uv_large[0], &ind_buf_large[0], &box_offsets[0], LARGE_VERT_BUF_BOXES);
+
+	struct vec4_float final_vert_buf[VERTS_IN_BOX];
+	struct vec4_float final_vert_buf2[VERTS_IN_BOX];
+	struct vec4_float final_vert_buf_large[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
+	struct vec4_float final_vert_buf_large2[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
+	struct vec4_float final_vert_buf_large3[VERTS_IN_BOX * LARGE_VERT_BUF_BOXES];
 	
 	/* Transfrom related */
 	struct vec3_float translation = { .x = -5.5f, .y = -8.0f, .z = 10.0f };
 	struct vec3_float camera_trans = { .x = 2.0f, .y = -4.0f, .z = 0.0f };
 	struct matrix_3x4 trans_mat = mat34_get_translation(&translation);
-	translation.x += 2.0f;
-	translation.z += 3.0f;
+	translation.x += 2.0f; translation.z += 3.0f;
 	struct matrix_3x4 trans_mat2 = mat34_get_translation(&translation);
-	translation.x += 2.0f;
-	translation.y += 4.0f;
-	translation.z += 15.0f;
+	translation.x += 2.0f; translation.y += 4.0f; translation.z += 15.0f;
 	struct matrix_3x4 trans_mat_large = mat34_get_translation(&translation);
-	translation.x += 26.0f;
-	translation.y -= 8.0f;
-	translation.z += 10.0f;
+	translation.x += 26.0f; translation.y -= 8.0f; translation.z += 10.0f;
 	struct matrix_3x4 trans_mat_large2 = mat34_get_translation(&translation);
-	translation.x += 10.0f;
-	translation.y += 18.0f;
-	translation.z += 50.0f;
+	translation.x += 10.0f; translation.y += 18.0f; translation.z += 50.0f;
 	struct matrix_3x4 trans_mat_large3 = mat34_get_translation(&translation);
 
+	void *back_buffer = get_backbuffer(renderer_info);
+	if (!back_buffer)
+		error_popup("Couldn't get back buffer", true);
+
 	struct vec2_int backbuffer_size = get_backbuffer_size(renderer_info);
-	struct matrix_4x4 perspective_mat = mat44_get_perspective_lh_fov(DEG_TO_RAD(59.0f), (float)backbuffer_size.x / (float)backbuffer_size.y, 1.0f, 1000.0f);
 	uint32_t *depth_buf = malloc(backbuffer_size.x * backbuffer_size.y * sizeof(uint32_t));
 
-	uint32_t frame_time_mus = 0;
+	struct matrix_4x4 perspective_mat = mat44_get_perspective_lh_fov(DEG_TO_RAD(59.0f), (float)backbuffer_size.x / (float)backbuffer_size.y, 1.0f, 1000.0f);
 
+	uint32_t frame_time_mus = 0;
 	uint64_t frame_start = get_time();
 
 	while (event_loop())
@@ -123,43 +114,28 @@ void main(struct api_info *api_info, struct renderer_info *renderer_info)
 
 		/* World space */
 		struct matrix_3x4 rot_mat = mat34_get_rotation_y(DEG_TO_RAD(40.0f));
-		struct matrix_3x4 world_transform = mat34_mul_mat34(&trans_mat, &rot_mat);
-		struct matrix_4x4 final_transform = mat44_mul_mat34(&camera_projection, &world_transform);
-		for (unsigned int i = 0; i < sizeof(vert_buf) / sizeof(vert_buf[0]); ++i)
-			final_vert_buf[i] = mat44_mul_vec3(&final_transform, &vert_buf[i]);
+		transform_vertices(&(vert_buf[0]), &(final_vert_buf[0]), sizeof(vert_buf) / sizeof(vert_buf[0]), &trans_mat, &rot_mat, &camera_projection);
 
-		/* A function for this stuff would be nice */
 		rot_mat = mat34_get_rotation_y(DEG_TO_RAD(38.0f));
-		world_transform = mat34_mul_mat34(&trans_mat2, &rot_mat);
-		final_transform = mat44_mul_mat34(&camera_projection, &world_transform);
-		for (unsigned int i = 0; i < sizeof(vert_buf) / sizeof(vert_buf[0]); ++i)
-			final_vert_buf2[i] = mat44_mul_vec3(&final_transform, &vert_buf[i]);
+		transform_vertices(&(vert_buf[0]), &(final_vert_buf2[0]), sizeof(vert_buf) / sizeof(vert_buf[0]), &trans_mat2, &rot_mat, &camera_projection);
 
 		rot_mat = mat34_get_rotation_y(DEG_TO_RAD(30.0f));
-		world_transform = mat34_mul_mat34(&trans_mat_large, &rot_mat);
-		final_transform = mat44_mul_mat34(&camera_projection, &world_transform);
-		for (unsigned int i = 0; i < sizeof(vert_buf_large) / sizeof(vert_buf_large[0]); ++i)
-			final_vert_buf_large[i] = mat44_mul_vec3(&final_transform, &vert_buf_large[i]);
+		transform_vertices(&(vert_buf_large[0]), &(final_vert_buf_large[0]), sizeof(vert_buf_large) / sizeof(vert_buf_large[0]), &trans_mat_large, &rot_mat, &camera_projection);
 
 		rot_mat = mat34_get_rotation_y(DEG_TO_RAD(-45.0f));
-		world_transform = mat34_mul_mat34(&trans_mat_large2, &rot_mat);
-		final_transform = mat44_mul_mat34(&camera_projection, &world_transform);
-		for (unsigned int i = 0; i < sizeof(vert_buf_large) / sizeof(vert_buf_large[0]); ++i)
-			final_vert_buf_large2[i] = mat44_mul_vec3(&final_transform, &vert_buf_large[i]);
+		transform_vertices(&(vert_buf_large[0]), &(final_vert_buf_large2[0]), sizeof(vert_buf_large) / sizeof(vert_buf_large[0]), &trans_mat_large2, &rot_mat, &camera_projection);
 
-		world_transform = mat34_mul_mat34(&trans_mat_large3, &rot_mat);
-		final_transform = mat44_mul_mat34(&camera_projection, &world_transform);
-		for (unsigned int i = 0; i < sizeof(vert_buf_large) / sizeof(vert_buf_large[0]); ++i)
-			final_vert_buf_large3[i] = mat44_mul_vec3(&final_transform, &vert_buf_large[i]);
+		transform_vertices(&(vert_buf_large[0]), &(final_vert_buf_large3[0]), sizeof(vert_buf_large) / sizeof(vert_buf_large[0]), &trans_mat_large3, &rot_mat, &camera_projection);
+
 
 		rasterizer_clear_depth_buffer(depth_buf, &backbuffer_size);
 
 		uint64_t raster_duration = get_time();
-		rasterizer_rasterize(get_backbuffer(renderer_info), depth_buf, &backbuffer_size, &final_vert_buf[0], &uv[0], &ind_buf[0], sizeof(ind_buf) / sizeof(ind_buf[0]), texture_data, texture_size);
-		rasterizer_rasterize(get_backbuffer(renderer_info), depth_buf, &backbuffer_size, &final_vert_buf2[0], &uv[0], &ind_buf[0], sizeof(ind_buf) / sizeof(ind_buf[0]), texture_data, texture_size);
-		rasterizer_rasterize(get_backbuffer(renderer_info), depth_buf, &backbuffer_size, &final_vert_buf_large[0], &uv_large[0], &ind_buf_large[0], sizeof(ind_buf_large) / sizeof(ind_buf_large[0]), texture_data, texture_size);
-		rasterizer_rasterize(get_backbuffer(renderer_info), depth_buf, &backbuffer_size, &final_vert_buf_large2[0], &uv_large[0], &ind_buf_large[0], sizeof(ind_buf_large) / sizeof(ind_buf_large[0]), texture_data, texture_size);
-		rasterizer_rasterize(get_backbuffer(renderer_info), depth_buf, &backbuffer_size, &final_vert_buf_large3[0], &uv_large[0], &ind_buf_large[0], sizeof(ind_buf_large) / sizeof(ind_buf_large[0]), texture_data, texture_size);
+		rasterizer_rasterize(back_buffer, depth_buf, &backbuffer_size, &final_vert_buf[0], &uv[0], &ind_buf[0], ind_buf_size, texture_data, texture_size);
+		rasterizer_rasterize(back_buffer, depth_buf, &backbuffer_size, &final_vert_buf2[0], &uv[0], &ind_buf[0], ind_buf_size, texture_data, texture_size);
+		rasterizer_rasterize(back_buffer, depth_buf, &backbuffer_size, &final_vert_buf_large[0], &uv_large[0], &ind_buf_large[0], ind_buf_large_size, texture_data, texture_size);
+		rasterizer_rasterize(back_buffer, depth_buf, &backbuffer_size, &final_vert_buf_large2[0], &uv_large[0], &ind_buf_large[0], ind_buf_large_size, texture_data, texture_size);
+		rasterizer_rasterize(back_buffer, depth_buf, &backbuffer_size, &final_vert_buf_large3[0], &uv_large[0], &ind_buf_large[0], ind_buf_large_size, texture_data, texture_size);
 		raster_duration = get_time() - raster_duration;
 
 		/* Stat rendering should be easy to disable/modify,
@@ -193,6 +169,21 @@ void main(struct api_info *api_info, struct renderer_info *renderer_info)
 	texture_destroy(&texture);
 }
 
+void transform_vertices(const struct vec3_float *in_verts, struct vec4_float *out_verts, unsigned int vert_count,
+                     struct matrix_3x4 *translation, struct matrix_3x4 *rotation, struct matrix_4x4 *camera_projection)
+{
+	assert(in_verts && "transform_vertices: in_verts is NULL");
+	assert(out_verts && "transform_vertices: out_verts is NULL");
+	assert(translation && "transform_vertices: translation is NULL");
+	assert(rotation && "transform_vertices: rotation is NULL");
+	assert(camera_projection && "transform_vertices: camera_projection is NULL");
+
+	struct matrix_3x4 world_transform = mat34_mul_mat34(translation, rotation);
+	struct matrix_4x4 final_transform = mat44_mul_mat34(camera_projection, &world_transform);
+	for (unsigned int i = 0; i < vert_count; ++i)
+		out_verts[i] = mat44_mul_vec3(&final_transform, &in_verts[i]);
+}
+
 void handle_input(struct api_info *api_info, float dt, struct vec3_float *camera_trans)
 {
 	assert(api_info && "handle_input: api_info is NULL");
@@ -209,6 +200,62 @@ void handle_input(struct api_info *api_info, float dt, struct vec3_float *camera
 		camera_trans->z += camera_speed * dt;
 	else if (is_key_down(api_info, KEY_S))
 		camera_trans->z -= camera_speed * dt;
+}
+
+void create_box_buffers(struct vec3_float *out_vert_buf, struct vec2_float *out_uv_buf, unsigned int *out_ind_buf)
+{
+	assert(out_vert_buf && "create_box_buffers: out_vert_buf is NULL");
+	assert(out_uv_buf && "create_box_buffers: out_uv_buf is NULL");
+	assert(out_ind_buf && "create_box_buffers: out_ind_buf is NULL");
+
+	out_vert_buf[0].x = -2.0f;  out_vert_buf[0].y = 2.0f;   out_vert_buf[0].z = 2.0f; 
+	out_vert_buf[1].x = -2.0f;  out_vert_buf[1].y = -2.0f;  out_vert_buf[1].z = 2.0f;
+	out_vert_buf[2].x = -2.0f;  out_vert_buf[2].y = 2.0f;   out_vert_buf[2].z = 2.0f;
+	out_vert_buf[3].x = -2.0f;  out_vert_buf[3].y = 2.0f;   out_vert_buf[3].z = -2.0f;
+	out_vert_buf[4].x = -2.0f;  out_vert_buf[4].y = -2.0f;  out_vert_buf[4].z = -2.0f;
+	out_vert_buf[5].x = -2.0f;  out_vert_buf[5].y = -2.0f;  out_vert_buf[5].z = 2.0f;
+	out_vert_buf[6].x = 2.0f;   out_vert_buf[6].y = 2.0f;   out_vert_buf[6].z = 2.0f;
+	out_vert_buf[7].x = 2.0f;   out_vert_buf[7].y = 2.0f;   out_vert_buf[7].z = -2.0f;
+	out_vert_buf[8].x = 2.0f;   out_vert_buf[8].y = -2.0f;  out_vert_buf[8].z = -2.0f;
+	out_vert_buf[9].x = 2.0f;   out_vert_buf[9].y = -2.0f;  out_vert_buf[9].z = 2.0f;
+	out_vert_buf[10].x = 2.0f;  out_vert_buf[10].y = 2.0f;  out_vert_buf[10].z = 2.0f;
+	out_vert_buf[11].x = 2.0f;  out_vert_buf[11].y = -2.0f; out_vert_buf[11].z = 2.0f;
+	out_vert_buf[12].x = -2.0f; out_vert_buf[12].y = 2.0f;  out_vert_buf[12].z = 2.0f;
+	out_vert_buf[13].x = -2.0f; out_vert_buf[13].y = -2.0f; out_vert_buf[13].z = 2.0f;
+
+	out_uv_buf[0].x = 0.0f; out_uv_buf[0].y = 0.33f;
+	out_uv_buf[1].x = 0.0f; out_uv_buf[1].y = 0.66f;
+	out_uv_buf[2].x = 0.25f; out_uv_buf[2].y = 0.0f;
+	out_uv_buf[3].x = 0.25f; out_uv_buf[3].y = 0.33f;
+	out_uv_buf[4].x = 0.25f; out_uv_buf[4].y = 0.66f;
+	out_uv_buf[5].x = 0.25f; out_uv_buf[5].y = 1.0f;
+	out_uv_buf[6].x = 0.5f; out_uv_buf[6].y = 0.0f;
+	out_uv_buf[7].x = 0.5f; out_uv_buf[7].y = 0.33f;
+	out_uv_buf[8].x = 0.5f; out_uv_buf[8].y = 0.66f;
+	out_uv_buf[9].x = 0.5f; out_uv_buf[9].y = 1.0f;
+	out_uv_buf[10].x = 0.75f; out_uv_buf[10].y = 0.33f;
+	out_uv_buf[11].x = 0.75f; out_uv_buf[11].y = 0.66f;
+	out_uv_buf[12].x = 1.0f; out_uv_buf[12].y = 0.33f;
+	out_uv_buf[13].x = 1.0f; out_uv_buf[13].y = 0.66f;
+
+	/* front */
+	out_ind_buf[0] = 3; out_ind_buf[1] =  4; out_ind_buf[2] = 7;
+	out_ind_buf[3] = 7; out_ind_buf[4] =  4; out_ind_buf[5] = 8; 
+	/* top */
+	out_ind_buf[6] = 2; out_ind_buf[7] = 3; out_ind_buf[8] = 6;
+	out_ind_buf[9] = 6; out_ind_buf[10] = 3; out_ind_buf[11] = 7;
+	/* bottom */
+	out_ind_buf[12] = 4; out_ind_buf[13] = 5; out_ind_buf[14] = 8;
+	out_ind_buf[15] = 8; out_ind_buf[16] = 5; out_ind_buf[17] = 9;
+	/* left */
+	out_ind_buf[18] = 0; out_ind_buf[19] = 1; out_ind_buf[20] = 3;
+	out_ind_buf[21] = 3; out_ind_buf[22] = 1; out_ind_buf[23] = 4;
+	/* right */
+	out_ind_buf[24] = 7; out_ind_buf[25] = 8; out_ind_buf[26] = 10;
+	out_ind_buf[27] = 10; out_ind_buf[28] = 8; out_ind_buf[29] = 11;
+	/* back */
+	out_ind_buf[30] = 10; out_ind_buf[31] = 11; out_ind_buf[32] = 12;
+	out_ind_buf[33] = 12; out_ind_buf[34] = 11; out_ind_buf[35] = 13;
 }
 
 void generate_large_test_buffers(const struct vec3_float *vert_buf_box, const struct vec2_float *uv_box, const unsigned int *ind_buf_box,
